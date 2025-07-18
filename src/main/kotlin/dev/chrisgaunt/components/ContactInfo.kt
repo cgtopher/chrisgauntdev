1e740de45077bd5787a4cf816c8a15b232fb4dce
Chris <chris.tgaunt@gmail>
2025-07-17 22:01:30 -0400

    removed phone number, why'd I do that?

--- src/main/kotlin/dev/chrisgaunt/components/ContactInfo.kt
+++ src/main/kotlin/dev/chrisgaunt/components/ContactInfo.kt
@@ -5,32 +5,11 @@ import react.dom.html.ReactHTML.p
 import react.dom.html.ReactHTML.a
 import react.dom.html.ReactHTML.img
 import emotion.react.css
-import web.cssom.*
 import csstype.*
 import dev.chrisgaunt.components.subcomponents.CenteredContainer
 
 val ContactInfo = FC<Props> {
     CenteredContainer {
-
-        p {
-            css {
-                margin = 5.px
-                fontSize = 1.rem
-                display = Display.flex
-                alignItems = AlignItems.center
-                gap = 8.px
-            }
-            img {
-                css {
-                    width = 20.px
-                    height = 20.px
-                    filter = "brightness(0) invert(1)".unsafeCast<Filter>() // Make icon white
-                }
-                src = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/icons/telephone.svg"
-                alt = "Phone"
-            }
-            +"480.628.0260"
-        }
 
         p {
             css {
